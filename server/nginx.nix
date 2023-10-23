b28817548fdc76a35b5e23e01f7a648ef783e29b
Lea Waller <lea@lea.science>
2023-10-23 18:06:57 +0200

    Add nginx config for MTA-STS

--- server/nginx.nix
+++ server/nginx.nix
@@ -1,15 +1,15 @@
 { lib, config, pkgs, ... }: {
-  imports = [
-    ./mta-sts.nix
-  ];
-  
+  imports = [ ./mta-sts.nix ];
+
   services = {
     nginx = {
       enable = true;
       enableReload = true;
       recommendedTlsSettings = true;
       recommendedOptimisation = true;
+      recommendedBrotliSettings = true;
       recommendedGzipSettings = true;
+      recommendedZstdSettings = true;
       recommendedProxySettings = true;
       virtualHosts = {
         "server.lea.science" = {
