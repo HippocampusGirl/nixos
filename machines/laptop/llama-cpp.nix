8a380d120c8a0e5cef4f99f46ff0f20d1b6a186b
Lea Waller <lea@lea.science>
2026-03-08 19:54:26 +0100

    Update to Qwen3.5-35B-A3B-GGUF

--- machines/laptop/llama-cpp.nix
+++ machines/laptop/llama-cpp.nix
@@ -14,8 +14,19 @@ in
     package = llama-cpp;
 
     model = pkgs.fetchurl {
-      url = "https://huggingface.co/ggml-org/Qwen3-Coder-30B-A3B-Instruct-Q8_0-GGUF/resolve/main/qwen3-coder-30b-a3b-instruct-q8_0.gguf";
-      sha256 = "sha256-8imT4pMYtbnsICb2tlgCpcqZs4q0hEqrg67YomzgD/Y=";
+      url = "https://huggingface.co/unsloth/Qwen3.5-35B-A3B-GGUF/resolve/main/Qwen3.5-35B-A3B-Q8_0.gguf";
+      sha256 = "sha256-OAiGbAFqsCtK2ya4c/cAiizdLAcEo5cEBQEZqwYx20Y=";
     };
+
+    extraFlags = [
+      "--no-mmap"
+      "--no-warmup"
+      "--ctx-size"
+      "65536"
+      "--n-gpu-layers"
+      "65536"
+      "--flash-attn"
+      "on"
+    ];
   };
 }
